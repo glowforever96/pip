d1e9c3cc5ef0aa786d5760cabfa24f4cd4f2721d
boyfromthewell <tnsdyd10@naver.com>
2025-03-19 19:47:59 +0900

    feat: link

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>home 화면 dd d</div>;
+  return (
+    <div>
+      home 화면 dd d<Link href="/todo">todo 페이지</Link>
+    </div>
+  );
 }
